140c8ca84f3bf5f443cb78fd116d61c85255d1e4
AhmedAbdLMoaty <63984192+AhmedAbdLMoaty@users.noreply.github.com>
2024-05-01 01:05:05 +0300

    NEW : CPU COLOUM CONSUMPTION

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import subprocess
 import os
 import csv
 import config
-from memory_profiler import profile
 import psutil
 
 def run_all_tsp_files_in_folder(folder_path):
@@ -12,13 +11,20 @@ def run_all_tsp_files_in_folder(folder_path):
             config.filename = absolute_path
             main()
 
-@profile
 def main():
     algorithms = ["PACS", "mmas", "racs", "acs"]
     for algorithm in algorithms:
         print(f"Running {algorithm}...")
         print(f"Results for {os.path.basename(config.filename)}:")
+        
+        # Measure CPU consumption before running the algorithm
+        before_cpu = psutil.cpu_percent(interval=0.1)
+
         result = subprocess.run(["python", f"{algorithm}.py", config.filename], capture_output=True, text=True)
+        
+        # Measure CPU consumption after running the algorithm
+        after_cpu = psutil.cpu_percent(interval=0.1)
+
         if result.stderr:
             print(f"Error running {algorithm}: {result.stderr}")
         else:
@@ -42,17 +48,19 @@ def main():
                     best_distance = line.split(":")[1].strip()
 
             memory_usage = psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024  # Memory usage in MB
-            write_to_csv(algorithm, os.path.basename(config.filename), name, comment, dimensions, best_tour, best_distance, memory_usage)
+            cpu_usage = after_cpu - before_cpu  # CPU consumption in percent
 
-def write_to_csv(algorithm, filename, name, comment, dimensions, best_tour, best_distance, memory_usage):
+            write_to_csv(algorithm, os.path.basename(config.filename), name, comment, dimensions, best_tour, best_distance, memory_usage, cpu_usage)
+
+def write_to_csv(algorithm, filename, name, comment, dimensions, best_tour, best_distance, memory_usage, cpu_usage):
     csv_filename = f"{algorithm}_output.csv"
     exists = os.path.isfile(csv_filename)
     with open(csv_filename, "a", newline='') as csvfile:
-        fieldnames = ['Name', 'Comment', 'Dimensions', 'Filename', 'Best Tour', 'Best Distance', 'Memory Usage (MB)']
+        fieldnames = ['Name', 'Comment', 'Dimensions', 'Filename', 'Best Tour', 'Best Distance', 'Memory Usage (MB)', 'CPU Consumption (%)']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         if not exists:
             writer.writeheader()
-        writer.writerow({'Name': name, 'Comment': comment, 'Dimensions': dimensions, 'Filename': filename, 'Best Tour': best_tour, 'Best Distance': best_distance, 'Memory Usage (MB)': memory_usage})
+        writer.writerow({'Name': name, 'Comment': comment, 'Dimensions': dimensions, 'Filename': filename, 'Best Tour': best_tour, 'Best Distance': best_distance, 'Memory Usage (MB)': memory_usage, 'CPU Consumption (%)': cpu_usage})
 
 
 if __name__ == "__main__":
